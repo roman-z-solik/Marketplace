89b541e5222989a60381c1bb35a05967fdb4b23a
Roman Z <roman-z@inbox.ru>
2025-08-26 14:27:29 +0500

    Write code app

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        return HttpResponse("Сообщение получено, {}!".format(name))
+    return render(request, 'contacts.html')
