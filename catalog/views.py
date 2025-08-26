fa4bf213eb790678bbfd959b784aa7b48d7bdb92
Roman Z <roman-z@inbox.ru>
2025-08-26 14:48:04 +0500

    Fill README.md, homework 22 ready

--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
 
         return HttpResponse("Сообщение получено, {}!".format(name))
-    return render(request, 'contacts.html')
+    return render(request, "contacts.html")
