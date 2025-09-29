1e5581b8fc24b6dc27e8d913f19dc3881ddff767
Roman Z <roman-z@inbox.ru>
2025-09-29 13:24:58 +0500

    Task 26.1 is ready

--- catalog/views.py
+++ catalog/views.py
@@ -14,17 +14,25 @@ from catalog.models import Product
 
 
 class ProductListView(ListView):
+    """Класс ProductListView(ListView) - это класс-представление на основе классов (CBV) в
+    Django для отображения списка продуктов."""
+
     model = Product
     template_name = "product_list.html"
 
 
 class ProductDetailView(DetailView):
+    """Класс ProductDetailView(DetailView) - это класс-представление для отображения
+    детальной информации об одном объекте."""
+
     form_class = ProductForm
     model = Product
     template_name = "product_detail.html"
 
 
 class ProductCreateView(CreateView):
+    """Класс ProductCreateView(CreateView) - это класс-представление для создания новых объектов."""
+
     model = Product
     template_name = "create_product.html"
     form_class = ProductForm
@@ -32,6 +40,9 @@ class ProductCreateView(CreateView):
 
 
 class ProductUpdateView(UpdateView):
+    """Класс ProductUpdateView(UpdateView) - это класс-представление для редактирования
+    существующих объектов."""
+
     model = Product
     template_name = "update_product.html"
     form_class = ProductForm
@@ -42,6 +53,8 @@ class ProductUpdateView(UpdateView):
 
 
 class ProductDeleteView(DeleteView):
+    """Класс ProductDeleteView(DeleteView) - это класс-представление для удаления объектов."""
+
     model = Product
     template_name = "delete_product.html"
     success_url = reverse_lazy("catalog:product_list")
