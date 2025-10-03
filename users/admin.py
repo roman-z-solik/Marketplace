60d184b3466493260ab321e5a3fc8e0b3629a68a
Roman Z <roman-z@inbox.ru>
2025-10-03 10:12:09 +0500

    Make some changes

--- users/admin.py
+++ users/admin.py
@@ -1,20 +1,2 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    # list_display = ['email', 'first_name', 'last_name', 'is_staff', 'is_active']
-    # list_filter = ['is_staff', 'is_active']
-    # fieldsets = UserAdmin.fieldsets + (
-    #     (None, {'fields': ('avatar', 'phone_number', 'country')}),
-    # )
-    # add_fieldsets = UserAdmin.add_fieldsets + (
-    #     (None, {'fields': ('avatar', 'phone_number', 'country')}),
-    # )
-    search_fields = ['email']
-    ordering = ['email']
+#
--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from users.apps import UsersConfig
 from users.views import UserCreateView
 
-app_name = UsersConfig.name
+app_name = 'users'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
